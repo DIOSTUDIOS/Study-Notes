9b140bd5e608ee3a5479cd38bb45ee230dac3aa2
Amos Wang <personalmail.wang@icloud.com>
2019-11-01 14:43:53 +0800

    2019.11.01.01

--- UrlsAutoPush/urls_auto_push.py
+++ UrlsAutoPush/urls_auto_push.py
@@ -1,5 +1,6 @@
 import requests
 import datetime
+import re
 
 def UrlsAutoPush():
     # 读取 urls.txt 中需要推送的链接
@@ -7,19 +8,29 @@ def UrlsAutoPush():
     # 将 urls 推送至百度，并接收百度返回的结果
     baiduResult = requests.post("http://data.zz.baidu.com/urls?site=https://www.diostudio.wang&token=IXUROgLSmZjHZm6P", files=urls)
 
+    if baiduResult.status_code == 200:
+    	pattern = r'{"remain":(.*?),"success":.*?,"not_valid":.*?'
+    	surplusQuantity = re.findall(pattern, baiduResult.text, re.S)
+
+    	pattern = r'{"remain":.*?,"success":(.*?),"not_valid":.*?'
+    	successQuantity = re.findall(pattern, baiduResult.text, re.S)
+    else:
+    	pass
+
     result = {
-            "surplusQuantity" : baiduResult.text[10:17], # 今日剩余推送数量
-            "successQuantity" : baiduResult.text[28:30]  # 本次成功推送数量,这里去几位数根据你的url数量自己调整就可以了
+            "surplusQuantity" : surplusQuantity[0], # 今日剩余推送数量
+            "successQuantity" : successQuantity[0]  # 本次成功推送数量,这里去几位数根据你的url数量自己调整就可以了
         }
 
     return result
+
 
 def FeedBack():
     resultFile = open("result.txt", "a")
     result = UrlsAutoPush()
     # 判断最高位是否大于0，无论成功还是失败都写入日志
     if int(result["successQuantity"]) > 0:
-        resultFile.write(str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + "\tSurplus URLs Quantities : " + result["surplusQuantity"] + "\tSuccess URLs Quantities : " + result["successQuantity"] + "\n")
+        resultFile.write(str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + "\tSurplus Urls Quantities : " + result["surplusQuantity"] + "\tSuccess Urls Quantities : " + result["successQuantity"] + "\n")
     else:
         resultFile.write(str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + "COME TO NOTHING!!!\n")
 
